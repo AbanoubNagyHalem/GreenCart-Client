a3779c8420b09b8f607a0f0454eb79b056817088
Abanoub Nagy <96650563+AbanoubNagyHalem@users.noreply.github.com>
2025-06-18 15:13:18 +0300

    Cart

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -52,6 +52,27 @@ export const AppContextProvidor = ({ children }) => {
     setCartItems(cartData);
   };
 
+  const getCartCount = () => {
+    let totalCount = 0;
+    for (const item in cartItems) {
+      totalCount += cartItems[item];
+    }
+    return totalCount;
+  };
+
+  const getCartAmount = () => {
+    let totalAmount = 0;
+    for (const items in cartItems) {
+      let itemInfo = products.find((product) => product._id === items);
+      console.log(itemInfo);
+
+      if (cartItems[items] > 0) {
+        totalAmount += itemInfo.offerPrice * cartItems[items];
+      }
+    }
+    return Math.floor(totalAmount * 100) / 100;
+  };
+
   useEffect(() => {
     fetchProducts();
   }, []);
@@ -72,6 +93,8 @@ export const AppContextProvidor = ({ children }) => {
     cartItems,
     searchQuery,
     setSearchQuery,
+    getCartAmount,
+    getCartCount,
   };
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
 };
--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -5,7 +5,7 @@ import { assets } from "../assets/assets";
 import ProductCard from "../components/ProductCard";
 
 const ProductDetails = () => {
-  const { products, navigate, currency, addToCard } = useAppContext();
+  const { products, navigate, currency, addToCart } = useAppContext();
   const { id } = useParams();
 
   const [relatedProducts, setRelatedProducts] = useState([]);
@@ -91,14 +91,14 @@ const ProductDetails = () => {
 
             <div className="flex items-center mt-10 gap-4 text-base">
               <button
-                onClick={() => addToCard(product._id)}
+                onClick={() => addToCart(product._id)}
                 className="w-full py-3.5 cursor-pointer font-medium bg-gray-100 text-gray-800/80 hover:bg-gray-200 transition"
               >
                 Add to Cart
               </button>
               <button
                 onClick={() => {
-                  addToCard(product._id);
+                  addToCart(product._id);
                   navigate("/cart");
                 }}
                 className="w-full py-3.5 cursor-pointer font-medium bg-primary text-white hover:bg-primary-dull transition"
